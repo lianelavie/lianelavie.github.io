5595048e382614ee85d34eede2edb67d9ad41ffc
lianelavie <lata.220221318@gehu.ac.in>
2025-05-03 23:55:22 +0530

    Added portfolio files

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './portfolio/**/*.html', // This includes all HTML files in the portfolio directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
